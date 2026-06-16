cea430f34a7e7b205e78b777d56720fc745b1921
Luiz Felipe Matias Xavier <luizfmxavier@gmail.com>
2026-06-16 20:58:13 -0300

    corrigindo o arquivo

---
+++ .history/ponteiro1_20260616205603.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// 1. Definição da struct Produto
+typedef struct {
+    int id;
+    float preco;
+} Produto;
+
+// 2. Função atualizada para receber o ponteiro da struct
+void aplicar_desconto(Produto *ptr_prod, float percentual) {
+    // O operador -> substitui a sintaxe (*ptr_prod).preco
+    // Ele acessa diretamente o campo 'preco' do produto apontado
+    ptr_prod->preco = ptr_prod->preco - (ptr_prod->preco * (percentual / 100.0f));
+}
+
+int main() {
+    // 3. Declarando e inicializando a struct Produto
+    Produto prod1;
+    prod1.id = 101;
+    prod1.preco = 100.0f; // Preço inicial
+    
+    float desconto = 15.0f; // 15% de desconto
+
+    printf("Produto ID: %d | Preco Original: R$ %.2f\n", prod1.id, prod1.preco);
+
+
+    aplicar_desconto(&prod1, desconto);
+
+
+    printf("Produto ID: %d | Preco com Desconto: R$ %.2f\n", prod1.id, prod1.preco);
+
+    return 0;
+}
